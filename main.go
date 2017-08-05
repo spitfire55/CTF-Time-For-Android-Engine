474daed349dcde0a21a0eaba11b3f2ddd860c0b7
spitfire55 <spitfire94155@gmail.com>
2017-08-04 22:54:50 -0400

    Temporarily disabled cron.yaml. Started list of how domains are laid out. Got basic HTMLParser working

--- main.go
+++ main.go
@@ -3,21 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	//"github.com/anaskhan96/soup"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/urlfetch"
-
+	"io/ioutil"
+	"log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-	client := urlfetch.Client(ctx)
-	resp, err := client.Get("https://www.google.com/")
+	client := &http.Client {
+		Transport: &urlfetch.Transport{
+			Context: ctx,
+			// local dev app server doesn't like Lets Encrypt certs...
+			AllowInvalidServerCertificate: appengine.IsDevAppServer(),
+		},
+	}
+	resp, err := client.Get("https://ctftime.org/api/v1/top/",)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "HTTP GET returned status %v", resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc := string(body)
+	fmt.Fprint(w, doc)
 }
 
 func main() {
