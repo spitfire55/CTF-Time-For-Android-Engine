c5b7d7fc3d5e0aa485c6ff9313e283f25e0e9e73
Spitfyre <spitfire@spitfy.re>
2017-08-19 09:04:59 -0400

    Fixed deployed auth

--- firebase.go
+++ firebase.go
@@ -4,39 +4,36 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/zabawaba99/firego"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"golang.org/x/oauth2/jwt"
-	"gopkg.in/zabawaba99/firego.v1"
+
+	"os"
 )
 
-type Firebase struct {
-	fbInstance *firego.Firebase
-	context    context.Context
-	jwtConfig  *jwt.Config
-}
-var fb Firebase
+var fb *firego.Firebase
 
 func connect(ctx context.Context) {
-	fb.jwtConfig = authenticate()
-	fb.context = ctx
-	fb.fbInstance = firego.New("https://ctf-time-for-android.firebaseio.com/", fb.jwtConfig.Client(fb.context))
+	auth := authenticate()
+	fb = firego.New(os.Getenv("FIREBASE_BASE"), auth.Client(ctx))
+
 }
 
 func saveAllRankings(teamRankings interface{}) {
-	if err := fb.fbInstance.Child("Rankings").Set(teamRankings); err != nil {
+	if err := fb.Child("Rankings").Set(teamRankings); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func saveCurrentRankings(teamRankings interface{}) {
-	if err := fb.fbInstance.Child("Rankings").Child("2017").Set(teamRankings); err != nil {
+	if err := fb.Child("Rankings").Child("2017").Set(teamRankings); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func authenticate() *jwt.Config {
-	d, err := ioutil.ReadFile("/home/spitfire/External/CTF-Time-For-Android/firebase-adminsdk-token.json")
+	d, err := ioutil.ReadFile("./creds.json")
 	if err != nil {
 		log.Fatal(err)
 	}
