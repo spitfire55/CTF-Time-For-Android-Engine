93fc35302d90cc53b00268ac21a9b3a958c1d3a6
Spitfyre <spitfire@spitfy.re>
2017-08-28 08:32:55 -0400

    Starting to write team JSON code

--- firebase.go
+++ firebase.go
@@ -7,6 +7,8 @@ import (
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/appengine/log"
+	"fmt"
+	"strconv"
 )
 
 var fb *firego.Firebase
@@ -32,4 +34,25 @@ func saveCurrentRankings(teamRankings interface{}, ctx context.Context) {
 	if err := fb.Child("Rankings/2017").Set(teamRankings); err != nil {
 		log.Errorf(ctx, err.Error())
 	}
+}
+
+func saveAllTeams(teams interface{}, ctx context.Context) {
+	if err:= fb.Child("Teams").Set(teams); err != nil {
+		log.Errorf(ctx, err.Error())
+	}
+}
+
+func saveNewTeam(team interface{}, ctx context.Context) {
+	highestNode := fmt.Sprintf("%d", fb.Child("TeamHighestNode"))
+	if err := fb.Child("Teams/" + highestNode).Set(team); err != nil {
+		log.Errorf(ctx, err.Error())
+	}
+	highestNodeInt, err := strconv.Atoi(highestNode)
+	if err != nil {
+		log.Errorf(ctx, err.Error())
+	}
+	if err := fb.Child("TeamHighestNode").Set( highestNodeInt+ 1); err != nil {
+		log.Errorf(ctx, err.Error())
+	}
+
 }
